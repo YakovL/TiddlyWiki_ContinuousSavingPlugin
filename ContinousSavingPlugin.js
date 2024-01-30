ebce3b60a470bd447608182fd55f6653985d188e
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-01-30 11:01:02 +0300

    infra: add the empty plugin

---
+++ ContinousSavingPlugin.js
@@ -0,0 +1,11 @@
+/***
+|Name        |ContinousSavingPlugin|
+|Description ||
+|Source      |https://github.com/YakovL/TiddlyWiki_ContinousSavingPlugin/blob/master/ContinousSavingPlugin.js|
+|Author      |Yakov Litvin|
+|Version     |0.0.0|
+|Contact     |Create an [[issue|https://github.com/YakovL/TiddlyWiki_ContinousSavingPlugin/issues]] or start a new thread in the [[Google Group|https://groups.google.com/g/tiddlywikiclassic/]]|
+|License     |[[MIT|https://github.com/YakovL/TiddlyWiki_ContinousSavingPlugin/blob/master/LICENSE]]|
+***/
+//{{{
+//}}}
